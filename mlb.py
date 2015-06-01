7e5df6b4ccdd700f01a06fbb5585f08feac68c12
HiMurakami <nbk.01409@gmail.com>
2015-06-02 03:18:31 +0900

    コロラドの勝ち負け数をストリング型で取得し表示

--- mlb.py
+++ mlb.py
@@ -1,5 +1,15 @@
 import urllib2
+import re
 fp = urllib2.urlopen('http://mlb.mlb.com/home')
+#fp = urllib2.urlopen('http://google.com')
 html = fp.read()
 fp.close()
-print html
+
+colw = "Colorado</td>\n<td class=\"td-w\">"
+print "COL:",
+#for i in range(30):
+print html[html.find(colw) + len(colw)], 
+print html[html.find(colw) + len(colw) + 1], 
+print ", ", 
+print html[html.find(colw) + len(colw) +25], 
+print html[html.find(colw) + len(colw) +26] 
