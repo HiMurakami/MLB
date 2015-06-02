85b6b8ec072204fea4b69d58acf307c6d6d30169
HiMurakami <nbk.01409@gmail.com>
2015-06-02 14:12:17 +0900

    MLB全チームの勝敗数表示

--- mlb.py
+++ mlb.py
@@ -1,15 +1,27 @@
 import urllib2
-import re
 fp = urllib2.urlopen('http://mlb.mlb.com/home')
-#fp = urllib2.urlopen('http://google.com')
 html = fp.read()
 fp.close()
 
-colw = "Colorado</td>\n<td class=\"td-w\">"
-print "COL:",
-#for i in range(30):
-print html[html.find(colw) + len(colw)], 
-print html[html.find(colw) + len(colw) + 1], 
-print ", ", 
-print html[html.find(colw) + len(colw) +25], 
-print html[html.find(colw) + len(colw) +26] 
+Aeast = ["Toronto","Boston","NY Yankees","Baltimore","Tampa Bay"]
+Acent = ["Detroit","Chi White Sox","Kansas City","Cleveland","Minnesota"]
+Awest = ["Seattle","Oakland","LA Angels","Texas","Houston"]
+Neast = ["Washington","NY Mets","Atlanta","Miami","Philadelphia"]
+Ncent = ["St. Louis","Chi Cubs","Pittsburgh","Cincinnati","Milwaukee"]
+Nwest = ["LA Dodgers","San Francisco","San Diego","Arizona","Colorado"]
+
+AL = [Aeast, Acent, Awest]
+NL = [Neast, Ncent, Nwest]
+
+TEAM = [AL, NL]
+
+for k in range(2):
+ for j in range(3):
+  for i in range(5):
+   a = TEAM[k][j][i]+"</td>\n<td class=\"td-w\">"
+   tmp1 = html[html.find(a) + len(a)]     + html[html.find(a) + len(a) + 1] + html[html.find(a) + len(a) + 2] 
+   tmp2 = html[html.find(a) + len(a) +25] + html[html.find(a) + len(a) +26] + html[html.find(a) + len(a) +27]
+   print TEAM[k][j][i] + ": ",
+   print int(tmp1.replace("<","").replace("t","")),
+   print int(tmp2.replace("<","").replace("t",""))
+
